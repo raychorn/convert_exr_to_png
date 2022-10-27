4365d8f5b4a1fe4cba287d0c25181e380a107be6
Ray C Horn <raychorn@gmail.com>
2022-10-27 12:53:38 -0600

    latest.

---
+++ makevenv.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+VENV=venv
+REQS=./requirements-fastapi.txt
+
+TARGET_PY="3.10"
+LOCAL_BIN=~/.local/bin
+
+DIR0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+MODE=$1
+echo "MODE: $MODE - use something for parm1 to run this script manually."
+
+python39=$(which python)
+
+if [ ! -f "$python39" ]; then
+    echo "python not found: $python39"
+    exit 1
+fi
+
+v=$($python39 -c 'import sys; i=sys.version_info; print("{}{}{}".format(i.major,i.minor,i.micro))')
+
+pip3=$(which pip3)
+setuptools="0"
+
+if [[ -f $python39 ]]
+then
+    pip_local=$LOCAL_BIN/pip3
+    if [[ -f $pip_local ]]
+    then
+        echo "8. Found $pip_local"
+        export PATH=$LOCAL_BIN:$PATH
+    else
+        echo "Must install PIP?"
+        if [[ -f $pip3 ]]
+        then
+            echo "9. $pip3 exists so not installing pip3, at this time."
+        else
+            echo "10. Installing pip3"
+            GETPIP=$DIR0/get-pip.py
+
+            if [[ ! -f $GETPIP ]]
+            then
+                echo "12. Downloading get-pip.py"
+                curl https://bootstrap.pypa.io/get-pip.py -o $GETPIP
+            fi
+
+            if [[ -f $GETPIP ]]
+            then
+                $python39 $GETPIP
+                export PATH=$LOCAL_BIN:$PATH
+                pip3=$(which pip3)
+                if [[ -f $pip3 ]]
+                then
+                    echo "11. Upgrading setuptools"
+                    setuptools="1"
+                    $pip3 install --upgrade setuptools > /dev/null 2>&1
+                fi
+            fi
+        fi
+    fi
+fi
+
+pip3=$(which pip3)
+echo "12. pip3 is $pip3"
+
+if [[ ! -f $pip3 ]]
+then
+    echo "13. Upgrading pip"
+    $pip3 install --upgrade pip > /dev/null 2>&1
+    if [[ "$setuptools." == "0." ]]
+    then
+        echo "14. Upgrading setuptools"
+        $pip3 install --upgrade setuptools > /dev/null 2>&1
+    fi
+fi
+
+virtualenv=$(which virtualenv)
+echo "15. virtualenv is $virtualenv"
+
+if [[ ! -f $virtualenv ]]
+then
+    echo "16. Installing virtualenv"
+    $pip3 install virtualenv > /dev/null 2>&1
+    $pip3 install --upgrade virtualenv > /dev/null 2>&1
+fi
+
+virtualenv=$(which virtualenv)
+echo "16. virtualenv is $virtualenv"
+
+if [[ ! -f $virtualenv ]]
+then
+    echo "17. Cannot find virtualenv ($virtualenv)"
+    exit 1
+fi
+
+choice=$python39
+
+version=$($choice --version)
+echo "Use this -> $choice --> $version"
+
+v=$($choice -c 'import sys; i=sys.version_info; print("{}{}{}".format(i.major,i.minor,i.micro))')
+vv=$($choice -c 'import sys; i=sys.version_info; print("{}.{}.{}".format(i.major,i.minor,i.micro))')
+echo "Use this -> $choice --> $v -> $vv"
+
+VENV=$VENV$v
+echo "VENV -> $VENV"
+
+if [[ -d $VENV ]]
+then
+    rm -R -f $VENV
+fi
+
+if [[ ! -d $VENV ]]
+then
+    echo "Making virtualenv for Python $choice -> $VENV"
+
+    if [[ ! -f "$choice" ]]
+    then
+        echo "Cannot find python:$choice"
+        exit 1
+    fi
+    if [[ ! -f "$virtualenv" ]]
+    then
+        echo "Cannot find virtualenv:$virtualenv"
+        exit 1
+    fi
+    echo "$virtualenv --python $choice -v $VENV"
+    $virtualenv --python $choice -v $VENV
+fi
+
+if [[ -d $VENV ]]
+then
+    . ./$VENV/bin/activate
+    pip install --upgrade setuptools > /dev/null 2>&1
+    pip install --upgrade pip > /dev/null 2>&1
+
+    if [[ -f $REQS ]]
+    then
+        echo "Installing $REQS"
+        pip install -r $REQS
+    fi
+
+fi
